b95a4847f0071e838d4a54b27c30cb40468653ee
tuobaling <wunhueic+hub@gmail.com>
2022-03-04 18:47:17 +0800

    Exif by NET Framework 4.7.2

---
+++ PhoneExifbyNET/PhoneExif.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhoneExif.aspx.cs" Inherits="PhoneExifbyNET.PhoneExif" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="HiddenField1" runat="server" />
+            <asp:HiddenField ID="HiddenField2" runat="server" />
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="ShowOrigialSzieAndXY" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="Resize" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="ShowResizeXY" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" runat="server" Text="WriteXY" OnClick="Button4_Click" />
+        </div>
+        <div>
+            Before：<asp:Label ID="Label1" runat="server"></asp:Label><br />
+            After：<asp:Label ID="Label2" runat="server"></asp:Label><br />
+            Write：<asp:Label ID="Label3" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PhoneExifbyNET/PhoneExif.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using ExifLibrary;
+
+namespace PhoneExifbyNET
+{
+    public partial class PhoneExif : System.Web.UI.Page
+    {
+        public string _savePath 
+        {
+            get { return HiddenField1.Value; }
+            set { HiddenField1.Value = value; }
+        }
+
+        public string _savePath2
+        {
+            get { return HiddenField2.Value; }
+            set { HiddenField2.Value = value; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string savePath = @"~/UploadImg/";
+
+            if (FileUpload1.HasFile)
+            {
+                // Get the name of the file to upload.
+                string fileName = FileUpload1.FileName;
+
+                // Append the name of the file to upload to the path.
+                savePath = Server.MapPath(savePath + fileName);
+
+                _savePath = savePath;
+
+                FileUpload1.SaveAs(savePath);
+
+                Label1.Text = GetPhotoEXIF(savePath);
+            }
+            else
+            {
+                Response.Write("alert('請上傳檔案！')");
+                return;
+            }
+
+
+        }
+
+        private string GetPhotoEXIF(string picPath)
+        {
+            var fileSize = new FileInfo(picPath).Length;
+
+            var file = ImageFile.FromFile(picPath);
+
+            // the type of the ISO speed rating tag value is unsigned short
+            // see documentation for tag data types
+            var isoTag = file.Properties.Get<ExifUShort>(ExifTag.ISOSpeedRatings);
+
+            // the flash tag's value is an enum
+            var flashTag = file.Properties.Get<ExifEnumProperty<Flash>>(ExifTag.Flash);
+
+            // GPS latitude is a custom type with three rational values
+            // representing degrees/minutes/seconds of the latitude 
+            var latTag = file.Properties.Get<GPSLatitudeLongitude>(ExifTag.GPSLatitude);
+
+            var lngTag = file.Properties.Get<GPSLatitudeLongitude>(ExifTag.GPSLongitude);
+
+            return "fileSize：" + fileSize + "<br />經度：" + latTag + "<br />" + "緯度：" + lngTag;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Image image = System.Drawing.Image.FromFile(_savePath);
+            float aspectRatio = (float)image.Size.Width / (float)image.Size.Height;
+            int newHeight = 200;
+            int newWidth = Convert.ToInt32(aspectRatio * newHeight);
+            Bitmap thumbBitmap = new Bitmap(newWidth, newHeight);
+            Graphics thumbGraph = Graphics.FromImage(thumbBitmap);
+
+            thumbGraph.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            thumbGraph.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            thumbGraph.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            _savePath2 = Server.MapPath("~/ResizeImg/" + new FileInfo(_savePath).Name);
+
+            var imageRectangle = new Rectangle(0, 0, newWidth, newHeight);
+            thumbGraph.DrawImage(image, imageRectangle);
+            thumbBitmap.Save(_savePath2, System.Drawing.Imaging.ImageFormat.Jpeg);
+            thumbGraph.Dispose();
+            thumbBitmap.Dispose();
+            image.Dispose();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Label2.Text = GetPhotoEXIF(_savePath2);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            var savePath = Server.MapPath("~/WriteImg/" + new FileInfo(_savePath2).Name);
+            var file = ImageFile.FromFile(_savePath2);
+
+
+            file.Properties.Set(ExifTag.GPSLatitude, 111, 222, 333);
+            file.Properties.Set(ExifTag.GPSLongitude, 111, 222, 333);
+
+            file.Save(savePath);
+
+            Label3.Text = GetPhotoEXIF(savePath);
+
+        }
+    }
+}
